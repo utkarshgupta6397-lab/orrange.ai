a434afaa1c9bcff26dfc6c0a46a7f3085d8d80f7
Utkarsh Gupta <utkarshgupta@Manojs-MacBook-Air-4.local>
2026-06-01 15:55:50 +0530

    Adjust founder image vertical positioning

--- components/about/MeetTheFounders.tsx
+++ components/about/MeetTheFounders.tsx
@@ -271,7 +271,7 @@ export default function MeetTheFounders() {
                         alt={founder.name}
                         width={160}
                         height={160}
-                        className="w-full h-full object-cover"
+                        className="w-full h-full object-cover object-[center_60%]"
                       />
                     </div>
                   </div>
